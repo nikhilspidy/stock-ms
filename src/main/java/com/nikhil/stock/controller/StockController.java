892d46b77ffe6396ffa8af7e4c5c442d757f9015
unknown <nikhil08514@gmail.com>
2020-04-25 16:52:23 +0530

    testing file reading

--- src/main/java/com/nikhil/stock/controller/StockController.java
+++ src/main/java/com/nikhil/stock/controller/StockController.java
@@ -1,5 +1,11 @@
 package com.nikhil.stock.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +15,31 @@ public class StockController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		try {
+            FileWriter writer = new FileWriter("MyFile.txt", true);
+            writer.write("Hello World");
+            writer.write("\r\n");   // write new line
+            writer.write("Good Bye!");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }		
+		
+		String s="";
+		 try {
+	            FileReader reader = new FileReader("MyFile.txt");
+	            int character;
+	 
+	            while ((character = reader.read()) != -1) {
+	                s+=(char) character;
+	            }
+	            reader.close();
+	 
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		
+		return s;
 	}
 
 }
